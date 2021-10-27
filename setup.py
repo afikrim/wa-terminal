06f3b98d85c1d66e0a399e86afc03973024dd58f
Aziz Fikri M <afikrim10@gmail.com>
2021-10-28 01:09:55 +0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="wa-terminal",
+    version="0.1",
+    packages=["whatsapp"],
+    package_dir={"": "whatsapp"},
+    url="https://github.com/afikrim/wa-terminal",
+    license="",
+    author="afikrim",
+    author_email="afikrim10@gmail.com",
+    description="Whatsapp using terminal",
+)
