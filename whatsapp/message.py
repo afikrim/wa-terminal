6a5e5b0f8e5462d8d73ba3bcec0f10cc7a89ead9
Aziz Fikri M <afikrim10@gmail.com>
2021-10-28 01:25:56 +0700

    move support to whatsapp

--- whatsapp/message.py
+++ whatsapp/message.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from support.validate import check_element
+from whatsapp.support.validate import check_element
 
 
 class Message(object):
