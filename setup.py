b3ee69a46efb019e60dcb8ac66069fda279cea91
Aziz Fikri M <afikrim10@gmail.com>
2021-10-28 01:20:55 +0700

    modify setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,40 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(
-    name="wa-terminal",
-    version="0.1",
-    packages=["whatsapp"],
-    package_dir={"": "whatsapp"},
-    url="https://github.com/afikrim/wa-terminal",
-    license="",
-    author="afikrim",
-    author_email="afikrim10@gmail.com",
-    description="Whatsapp using terminal",
-)
+
+def get_requirements():
+    requirements_list = []
+
+    with open("requirements.txt") as reqs:
+        for install in reqs:
+            requirements_list.append(install.strip())
+
+    return requirements_list
+
+
+def get_packages_and_requirements():
+    reqs = get_requirements()
+
+    exclude = ["support*"]
+    packs = find_packages(exclude=exclude)
+
+    return packs, reqs
+
+
+def main():
+    packages, requirements = get_packages_and_requirements()
+
+    setup(
+        name="wa-terminal",
+        version="0.1",
+        packages=packages,
+        url="https://github.com/afikrim/wa-terminal",
+        license="",
+        author="afikrim",
+        author_email="afikrim10@gmail.com",
+        description="Whatsapp using terminal",
+        install_requires=requirements,
+    )
+
+
+if __name__ == "__main__":
+    main()
